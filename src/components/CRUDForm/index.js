e3e3138f5f2c790fa609c4d68fe8217f4e01ad8e
Benjamin Wilkins <b.wilkins@attineos.com>
2020-06-17 16:55:35 +0200

    feature : Added possibility to add an action on submit & changed to butt

--- src/components/CRUDForm/index.js
+++ src/components/CRUDForm/index.js
@@ -9,8 +9,7 @@ import RestFetcher from './RestFetcher'
 
 export const CRUDFORM_MODE_NEW = "MODE_NEW"
 export const CRUDFORM_MODE_UPDATE = "MODE_UPDATE"
-export const CRUDFORM_MODE_DELETE = "MODE_DELETE"
-export const CRUDFORM_MODES = [CRUDFORM_MODE_NEW, CRUDFORM_MODE_UPDATE, CRUDFORM_MODE_DELETE]
+export const CRUDFORM_MODES = [CRUDFORM_MODE_NEW, CRUDFORM_MODE_UPDATE]
 
 class CRUDForm extends Component {
 
@@ -18,39 +17,6 @@ class CRUDForm extends Component {
     isLoaded: this.props.mode === CRUDFORM_MODE_NEW,
     data: {},
     errors: {},
-  }
-
-  getFetcher() {
-    const CustomFetcher = this.props.customFetcher? this.props.customFetcher: RestFetcher
-    return new CustomFetcher(this.props.config)
-  }
-
-  handleDataChange = data => this.setState({ data })
-
-  handleSubmit = () => {
-    const fetcher = this.getFetcher()
-
-    if (this.props.mode === CRUDFORM_MODE_NEW) {
-      fetcher.createData(this.state.data).then(data => {
-        this.props.onCreateSuccess(data)
-      }, err => {
-        this.setState({ errors: this.props.onCreateError(err) })
-      })
-    }
-    if (this.props.mode === CRUDFORM_MODE_UPDATE) {
-      fetcher.updateData(this.state.data).then(data => {
-        this.props.onUpdateSuccess(data)
-      }, err => {
-        this.setState({ errors: this.props.onUpdateError(err) })
-      })
-    }
-    if (this.props.mode === CRUDFORM_MODE_DELETE) {
-      fetcher.deleteData(this.state.data).then(data => {
-        this.props.onDeleteSuccess(data)
-      }, err => {
-        this.setState({errors: this.props.onDeleteError(err) })
-      })
-    }
   }
 
   componentDidMount() {
@@ -64,6 +30,50 @@ class CRUDForm extends Component {
     })
   }
 
+  handleDataChange = data => this.setState({ data })
+
+  handleSubmit = () => {
+    const fetcher = this.getFetcher()
+
+    if (this.props.mode === CRUDFORM_MODE_NEW) {
+      fetcher.createData(this.state.data).then(data => {
+        this.props.onCreateSuccess(data)
+        if(this.props.submitAction() !== undefined) {
+          this.props.submitAction()
+        }
+      }, err => {
+        this.setState({ errors: this.props.onCreateError(err) })
+      })
+    }
+    if (this.props.mode === CRUDFORM_MODE_UPDATE) {
+      fetcher.updateData(this.state.data).then(data => {
+        this.props.onUpdateSuccess(data)
+        if(this.props.submitAction() !== undefined) {
+          this.props.submitAction()
+        }
+      }, err => {
+        this.setState({ errors: this.props.onUpdateError(err) })
+      })
+    }
+  }
+
+  handleDelete = ()  => {
+    const fetcher = this.getFetcher()
+    fetcher.deleteData(this.state.data).then(data => {
+      this.props.onDeleteSuccess(data)
+      if(this.props.submitAction() !== undefined) {
+        this.props.submitAction()
+      }
+    }, err => {
+      this.setState({errors: this.props.onDeleteError(err) })
+    })
+  }
+
+  getFetcher() {
+    const CustomFetcher = this.props.customFetcher? this.props.customFetcher: RestFetcher
+    return new CustomFetcher(this.props.config)
+  }
+
   render() {
     if (!this.state.isLoaded) {
       return <Loader />
@@ -74,6 +84,7 @@ class CRUDForm extends Component {
       data={ this.state.data }
       onDataChange={ this.handleDataChange }
       onSubmit={ this.handleSubmit }
+      onDelete={ this.handleDelete }
     />
   }
 }
@@ -81,6 +92,8 @@ class CRUDForm extends Component {
 CRUDForm.propTypes = {
   mode: PropTypes.oneOf(CRUDFORM_MODES).isRequired,
   config: PropTypes.object.isRequired,
+  submitAction : PropTypes.func,
+  customFetcher: PropTypes.any,
 
   onCreateSuccess: PropTypes.func,
   onUpdateSuccess: PropTypes.func,
